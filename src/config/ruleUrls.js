4c13b589465341572c3c8824a36c68ba387219d7
charlo <wcl98854@gmail.com>
2026-06-16 15:41:32 +0800

    新分支部署

--- src/config/ruleUrls.js
+++ src/config/ruleUrls.js
@@ -9,3 +9,4 @@ export const CLASH_SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github
 export const CLASH_IP_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geoip/';
 export const SURGE_SITE_RULE_SET_BASEURL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geosite/'
 export const SURGE_IP_RULE_SET_BASEURL = 'https://gh-proxy.com/https://github.com/NSZA156/surge-geox-rules/raw/refs/heads/release/geo/geoip/'
+
